ecdbbde7cf445fb5b2df515f76519bf4e1a65822
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2025-10-29 01:13:38 -1000

    initial import

---
+++ lib/gepa_ex.ex
@@ -0,0 +1,18 @@
+defmodule GepaEx do
+  @moduledoc """
+  Documentation for `GepaEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GepaEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gepa_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule GepaExTest do
+  use ExUnit.Case
+  doctest GepaEx
+
+  test "greets the world" do
+    assert GepaEx.hello() == :world
+  end
+end
